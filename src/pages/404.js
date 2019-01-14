69a54b99cbef6110733d659a9c3f49ed36339523
Luong Dang Hai <hailuongdang1998@gmail.com>
2019-01-14 07:36:34 +0200

    upgraded 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
+import { Link } from 'gatsby'
 
 class NotFoundPage extends React.Component {
   render() {
@@ -9,7 +10,11 @@ class NotFoundPage extends React.Component {
       <Layout location={this.props.location}>
         <SEO title="404: Not Found" />
         <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <p>
+          Hey, the link you request does not exist... Maybe you can visit the{' '}
+          <Link to="/">home page</Link> and see if you can find what you want?
+          :)
+        </p>
       </Layout>
     )
   }
